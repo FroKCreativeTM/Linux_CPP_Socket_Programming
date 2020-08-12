6019ea31cac12f0256f9721e75192aae9c9dab15
seungCheolCha <cscculture95@gmail.com>
2020-08-13 00:47:56 +0900

    도메인 공부

--- ch08/gethostbyname.c
+++ ch08/gethostbyname.c
@@ -15,7 +15,25 @@ int main(int argc, char ** argv) {
 	struct hostent* host;
 
 	if(argc != 2) {
-		printf("");
+		printf("Usage : %s <addr> \n", argv[0]);
+		exit(1);
+	}
+
+	host = gethostbyname(argv[1]);
+	if(!host) {
+		error_handling("gethost... error");
+	}
+
+	printf("Official name : %s \n", host->h_name);
+	for(i = 0; host->h_aliases[i]; i++) {
+		printf("Aliases %d : %s\n", i + 1, host->h_aliases[i]);
+	}
+
+	printf("address type : %s \n", (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
+
+	for(i = 0; host->h_addr_list[i]; i++) {
+		printf("IP addr %d : %s\n", i + 1, inet_ntoa(*(struct in_addr*)host->h_addr_list[i]));
+	}
 
 	return 0;
 }
